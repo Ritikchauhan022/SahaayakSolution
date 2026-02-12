6385def5661a2c8f34d8c5d352fc7fce266c39e6
Ritik Chauhan <ritikchauhan@gmail.com>
2026-02-12 20:13:03 +0530

    LoginSignup page responsive fix

--- client/src/LoginSignup.js
+++ client/src/LoginSignup.js
@@ -142,7 +142,7 @@ function LoginSignup(props){
                  <button className="ls-back-btn" onClick={handleBack}>
                      <FaArrowLeft /> Back
                  </button>
-                 <h2>BakeryConnect</h2>
+                 <h2>SahaayakSolution</h2>
              </div>
 
               {/* === MAIN CARD === */}
